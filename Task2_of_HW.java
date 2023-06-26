ebf147a5bd73c56bc1ffa62a5fba54891dbb5cea
Nickolay Grigoriev <gnarus@inbox.ru>
2023-06-26 18:12:47 +0300

    4 commit (add: log of errors)

--- Task2_of_HW.java
+++ Task2_of_HW.java
@@ -3,22 +3,33 @@
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Formatter;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Task2_of_HW {
     public static void main(String[] args) throws IOException {
+        try {
+            int num1 = num();
+            int num2 = num();
+            char sign = Operation();
+            if ((sign == '*') || (sign == '/') || (sign == '+') || (sign == '-')) {
+                int result = Rezult(num1, num2, sign);
+                System.out.println("Результат операции: " + result);
+                logic(num1, num2, sign, result); // вызывает функцию которая проводит логирование результата вычисления
+            } else {
+                // вызывает функцию в которой проводится логирование на ошибку вводи символа
+                Object id = "java.util.NumberFormatException";
+                logic_of_error(id);
+            }
 
-        int num1 = num();
-        int num2 = num();
-        char sign = Operation();
-        int result = Rezult(num1, num2, sign);
-        System.out.println("Результат операции: " + result);
-        logic(num1, num2, sign, result);
+        } catch (NoSuchElementException id) {
+            // вызывает функцию в которой проводится логирование на ошибку ввода числа/цифры
+            logic_of_error(id);
+        }
     }
 
     public static int num() {
-
         System.out.println("Введите число: ");
         Scanner scanner = new Scanner(System.in);
         int result = scanner.nextInt();
@@ -32,7 +43,7 @@ public class Task2_of_HW {
         return result;
     }
 
-    public static int Rezult(int num1, int num2, char sign) {
+    public static int Rezult(int num1, int num2, char sign) throws IOException {
         int result = 0;
         switch (sign) {
             case '+':
@@ -48,11 +59,12 @@ public class Task2_of_HW {
                 result = num1 / num2;
                 break;
             default:
-                System.out.println("Неверный ввод");
+                break;
         }
         return result;
     }
 
+    // функция логирования результата ввода
     public static void logic(int num1, int num2, char sim, int result) throws IOException {
         String messeg = " ";
         Logger logger1 = Logger.getAnonymousLogger();
@@ -66,5 +78,16 @@ public class Task2_of_HW {
 
     }
 
-
+    // функция логирования ошибки ввода
+    public static void logic_of_error(Object id) throws IOException {
+        String messeg = " ";
+        Logger logger1 = Logger.getAnonymousLogger();
+        FileWriter filewriter1 = new FileWriter("log.txt");
+        Formatter f = new Formatter();
+        f.format("Error!!! ");
+        messeg = String.valueOf(f);
+        logger1.info(messeg);
+        filewriter1.write(messeg + " Name: " + id + " \n");
+        filewriter1.close();
+    }
 }
